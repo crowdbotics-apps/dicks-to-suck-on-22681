f34ef02f60d41683eda7e928e51c7ecb3e426fe4
Crowdbotics <team@crowdbotics.com>
2020-11-16 21:13:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dicks_to_suck_on_22681/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dicks_to_suck_on_22681;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "dicks_to_suck_on_22681";
+    }
+}
---
+++ ios/dicks_to_suck_on_22681Tests/dicks_to_suck_on_22681Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface dicks_to_suck_on_22681 : XCTestCase
+
+@end
+
+@implementation dicks_to_suck_on_22681
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
